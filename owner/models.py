9966cb43d32a9cf689633c67c09facf3ce3157b3
ttakku-park <ttakku.park@gmail.com>
2021-05-25 21:14:05 +0900

    CREATE : owner app 생성 및 migrate

--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
 # Create your models here.
+
+class Owner(models.Model):
+    
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=300)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'owners'
