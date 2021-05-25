bfdbe3d947b5e96899f203ce63afbfa95653f7ed
ttakku-park <ttakku.park@gmail.com>
2021-05-25 21:24:07 +0900

    CREATE : models.py에 Dog class 추가 (dogs)

--- owner/models.py
+++ owner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.fields import IntegerField
 # Create your models here.
 
 class Owner(models.Model):
@@ -10,3 +11,12 @@ class Owner(models.Model):
 
     class Meta:
         db_table = 'owners'
+
+class Dog(models.Model):
+
+    owner_id = models.ForeignKey('Owner',on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'dogs'
